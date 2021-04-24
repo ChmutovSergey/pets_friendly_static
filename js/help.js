8170d6885be40f3767a3e10d1accb3f2083a9fb4
AlexandrCh04 <a.chertikhin0072@mail.ru>
2021-04-24 12:15:35 +0500

    Выполнил все потребности валидации формы

--- js/help.js
+++ js/help.js
@@ -1,12 +1,82 @@
 const checkbox = document.querySelector("#agreement");
 const btn = document.querySelector("#send");
+const textarea = document.querySelector('#msg');
+const nameI = document.querySelector('#name');
+const email = document.querySelector('#email');
+let correctText = false;
+let correctCheck = false;
+let correctName = false;
+let correctEmail = false;
 
-checkbox.addEventListener('change', () => {
-  if (!checkbox.checked) {
-    btn.disabled = true;
-    btn.classList.add('blocked');
+
+textarea.oninput = () => {
+  if (textarea.value.length < 15) {
+    textarea.classList.add('error');
+    correctText = false;
   } else {
+    textarea.classList.remove('error');
+    correctText = true;
+  }
+
+  if (correctText && correctName && correctCheck && correctEmail) {
     btn.disabled = false;
     btn.classList.remove('blocked');
+  } else {
+    btn.disabled = true;
+    btn.classList.add('blocked');
   }
-})
+}
+
+nameI.oninput = () => {
+  if (nameI.value.length <= 1) {
+    nameI.classList.add('error');
+    correctName = false;
+  } else {
+    nameI.classList.remove('error');
+    correctName = true;
+  }
+
+  if (correctText && correctName && correctCheck && correctEmail) {
+    btn.disabled = false;
+    btn.classList.remove('blocked');
+  } else {
+    btn.disabled = true;
+    btn.classList.add('blocked');
+  }
+}
+
+email.oninput = () => {
+  const regexp = /@/gi;
+  const emailText = email.value;
+  correctEmail = regexp.test(emailText);
+
+  if (!correctEmail) {
+    email.classList.add('error');
+  } else {
+    email.classList.remove('error');
+  }
+
+  if (correctText && correctName && correctCheck && correctEmail) {
+    btn.disabled = false;
+    btn.classList.remove('blocked');
+  } else {
+    btn.disabled = true;
+    btn.classList.add('blocked');
+  }
+}
+
+checkbox.onchange = () => {
+  if (!checkbox.checked) {
+    correctCheck = false;
+  } else {
+    correctCheck = true;
+  }
+
+  if (correctText && correctName && correctCheck && correctEmail) {
+    btn.disabled = false;
+    btn.classList.remove('blocked');
+  } else {
+    btn.disabled = true;
+    btn.classList.add('blocked');
+  }
+}
